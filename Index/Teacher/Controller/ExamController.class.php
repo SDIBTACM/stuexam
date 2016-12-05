eed02ba945e17df7c02c34911dde8fddc7711d65
wilsonjuxta <wilsonjuxta@gmail.com>
2016-12-05 20:55:46 +0800

    廢棄多餘的js

--- Index/Teacher/Controller/ExamController.class.php
+++ Index/Teacher/Controller/ExamController.class.php
@@ -224,13 +224,15 @@ class ExamController extends TemplateController
         $acCount = M()->query($query);
         foreach($acCount as $ac) {
             $programCount[$ac['user_id']] = $ac['cnt'];
+            $users[] = $ac['user_id'];
         }
 
         foreach ($programRank as $p) {
             $userRank[$p['user_id']][$p['question_id']] = $p['answer'];
-            $users[] = $p['user_id'];
+            if (!in_array($p['user_id'], $users)) {
+                $users[] = $p['user_id'];
+            }
         }
-        $users = array_unique($users);
 
         $userIds_chunk = array_chunk($users, 50);
         foreach ($userIds_chunk as $_userIds) {
