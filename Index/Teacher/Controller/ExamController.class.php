76a8fca56c761c552948dc9b26c6e84793a5085a
wilsonjuxta <wilsonjuxta@gmail.com>
2017-12-04 02:13:13 +0800

    bug fixed

--- Index/Teacher/Controller/ExamController.class.php
+++ Index/Teacher/Controller/ExamController.class.php
@@ -170,7 +170,7 @@ class ExamController extends TemplateController
         $query = "SELECT COUNT(1) as totalNum FROM ex_privilege, users WHERE ex_privilege.user_id = users.user_id AND " .
             "ex_privilege.rightstr = 'e$this->eid' $sqladd";
         $numRow = M()->query($query);
-        $totalnum = $numRow['totalNum'];
+        $totalnum = empty($numRow) ? 0 : $numRow[0]['totalNum'];
 
         $query = "SELECT COUNT(*) as `realnum`,MAX(`choosesum`) as `choosemax`,MAX(`judgesum`) as `judgemax`,MAX(`fillsum`) as `fillmax`,".
 				"MAX(`programsum`) as `programmax`,MIN(`choosesum`) as `choosemin`,MIN(`judgesum`) as `judgemin`,MIN(`fillsum`) as `fillmin`,".
