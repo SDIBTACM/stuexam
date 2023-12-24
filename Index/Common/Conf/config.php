0e0c7626950ac1c2f45629ebd4ef4c4e68c5d99d
jiaying <jiaying@fordeal.com>
2023-12-24 14:50:28 +0800

    session key配置放到 config 中配置，不要记那么多文件了

--- Index/Common/Conf/config.php
+++ Index/Common/Conf/config.php
@@ -2,20 +2,30 @@
 return array(
     'LOAD_EXT_CONFIG' => 'log,database',
 
-	'OJ_VIP_CONTEST'=>false,
+    'OJ_VIP_CONTEST' => false,
 
-	'URL_HTML_SUFFIX' => '', //伪静态后缀名设置
-	'TMPL_VAR_IDENTIFY' => 'array', // 点语法的解析
-	'URL_MODEL' => 2,
+    'URL_HTML_SUFFIX' => '', //伪静态后缀名设置
+    'TMPL_VAR_IDENTIFY' => 'array', // 点语法的解析
+    'URL_MODEL' => 2,
 
-	'EACH_PAGE' => 20, //每一页显示的数目
-	'PAGE_NUM' => 10, //最多显示的页数
+    'EACH_PAGE' => 20, //每一页显示的数目
+    'PAGE_NUM' => 10, //最多显示的页数
 
-	//'SHOW_PAGE_TRACE' => true,
-	'URL_CASE_INSENSITIVE'  =>  false,
-    'LOG_LEVEL'  =>'EMERG,ALERT,CRIT,ERR',
+    //'SHOW_PAGE_TRACE' => true,
+    'URL_CASE_INSENSITIVE' => false,
+    'LOG_LEVEL' => 'EMERG,ALERT,CRIT,ERR',
 
-	'EXAM_VERSION' => '1.0.2',
+    'EXAM_VERSION' => '1.0.2',
 
-	'TEACHER_LIST_CACHE_TIME' => 86400 // 默认一天
+    'TEACHER_LIST_CACHE_TIME' => 86400, // 默认一天
+
+    // 考试系统所使用的 session key
+    'EXAM_SESSION_KEY' => array(
+        'USER_ID' => 'user_id',
+        'ADMINISTRATOR' => 'administrator',
+        'CONTEST_CREATOR' => 'contest_creator',
+        'PROBLEM_EDITOR' => 'problem_editor',
+        'GET_KEY' => 'getkey',
+        'POST_KEY' => 'postkey'
+    )
 );
--- Index/Home/Helper/SessionHelper.class.php
+++ Index/Home/Helper/SessionHelper.class.php
@@ -4,6 +4,7 @@ namespace Home\Helper;
 
 /**
  * 统一获取 session 中绑定信息的地方，防止后续 session 名称修改需要关联修改很多地方
+ * 修改 session key统一到配置文件中 Index/Common/Conf/config.php 修改
  * Class SessionHelper
  *
  * @package \Home\Helper
@@ -11,34 +12,34 @@ namespace Home\Helper;
 class SessionHelper {
 
     public static function getUserId() {
-        return session("user_id");
+        return session(C("EXAM_SESSION_KEY.USER_ID"));
     }
 
     public static function getAdministrator() {
-        return session("administrator");
+        return session(C("EXAM_SESSION_KEY.ADMINISTRATOR"));
     }
 
     public static function getContestCreator() {
-        return session("contest_creator");
+        return session(C("EXAM_SESSION_KEY.CONTEST_CREATOR"));
     }
 
     public static function getProblemEditor() {
-        return session("problem_editor");
+        return session(C("EXAM_SESSION_KEY.PROBLEM_EDITOR"));
     }
 
     public static function getCsrfGetKey() {
-        return $_SESSION['getkey'];
+        return $_SESSION[C("EXAM_SESSION_KEY.GET_KEY")];
     }
 
     public static function getCsrfPostKey() {
-        return $_SESSION['postkey'];
+        return $_SESSION[C("EXAM_SESSION_KEY.POST_KEY")];
     }
 
     public static function setCsrfGetKey($value) {
-        $_SESSION['getkey'] = $value;
+        $_SESSION[C("EXAM_SESSION_KEY.GET_KEY")] = $value;
     }
 
     public static function setCsrfPostKey($value) {
-        $_SESSION['postkey'] = $value;
+        $_SESSION[C("EXAM_SESSION_KEY.POST_KEY")] = $value;
     }
 }
